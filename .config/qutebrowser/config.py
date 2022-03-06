39dc5b50afac843a6db3fdc8fbc3545539a15812
Dorovich <silverdovi25@gmail.com>
2022-03-06 04:27:38 +0100

    qutebrowser

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,3 +1,22 @@
+# Youtube adblock
+##############################################################
+import subprocess
+import os
+from qutebrowser.api import interceptor
+
+def filter_yt(info: interceptor.Request):
+    """Block the given request if necessary."""
+    url = info.request_url
+    if (
+        url.host() == "www.youtube.com"
+        and url.path() == "/get_video_info"
+        and "&adformat=" in url.query()
+    ):
+        info.block()
+
+interceptor.register(filter_yt)
+##############################################################
+
 # Whether qutebrowser should also read 'autoconfig.yaml'
 config.load_autoconfig(False)
 
