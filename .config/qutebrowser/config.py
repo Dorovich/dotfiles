35608a4cf778040ba1b620231faff3661d892ff5
Dorovich <silverdovi25@gmail.com>
2022-03-07 13:41:13 +0100

    qutebrowser

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,49 +1,40 @@
-# Youtube adblock
-##############################################################
-import subprocess
-import os
-from qutebrowser.api import interceptor
-
-def filter_yt(info: interceptor.Request):
-    """Block the given request if necessary."""
-    url = info.request_url
-    if (
-        url.host() == "www.youtube.com"
-        and url.path() == "/get_video_info"
-        and "&adformat=" in url.query()
-    ):
-        info.block()
-
-interceptor.register(filter_yt)
-##############################################################
-
-# Whether qutebrowser should also read 'autoconfig.yaml'
+# general
 config.load_autoconfig(False)
-
-# Privacy config
-c.content.geolocation = False
-c.content.headers.user_agent = "Mozilla/5.0 ({os_info}; rv:84.0) Gecko/20100101 Firefox/84.0"
-c.content.canvas_reading = False
-c.content.webgl = False
-
-# Autoplay embed video
+c.downloads.location.prompt = False
+c.downloads.location.directory = "$HOME/Descargas"
+c.confirm_quit = ["downloads"]
 c.content.autoplay = False
+c.content.pdfjs = True
+c.hints.chars = "asdfghjklie"
+c.tabs.show = "multiple"
+c.tabs.last_close = "close"
 
-# General permissions
-config.set('content.images', True, 'chrome-devtools://*')
-config.set('content.images', True, 'devtools://*')
-config.set('content.javascript.enabled', True, 'chrome-devtools://*')
-config.set('content.javascript.enabled', True, 'devtools://*')
-config.set('content.javascript.enabled', True, 'chrome://*/*')
-config.set('content.javascript.enabled', True, 'qute://*/*')
-
-# Darkmode on websites
+# ui
+config.source("colors.py")
+c.colors.webpage.prefers_color_scheme_dark = True
 c.colors.webpage.darkmode.enabled = True
 c.colors.webpage.darkmode.algorithm = 'lightness-cielab'
 c.colors.webpage.darkmode.policy.page = 'smart'
+c.completion.show = "auto"
+c.completion.use_best_match = True
+c.scrolling.smooth = True
+c.completion.height = "20%"
+c.completion.quick = False
+c.window.title_format = "{private}{perc}qutebrowser"
+c.tabs.padding = {
+    "left": 5,
+    "right": 5,
+    "top": 1,
+    "bottom": 1,
+}
 
-# Content blocking
-# Use 'pip install adblock' to be able to use brave adblock ('both' method)
+# privacy
+c.content.webrtc_ip_handling_policy = "default-public-interface-only"
+c.content.geolocation = False
+c.content.headers.user_agent = "Mozilla/5.0 ({os_info}; rv:97.0) Gecko/20100101 Firefox/97.0"
+c.content.canvas_reading = False
+c.content.webgl = False
+c.content.cookies.accept = "no-unknown-3rdparty"
 c.content.blocking.enabled = True
 c.content.blocking.method = 'both'
 c.content.blocking.adblock.lists = [ \
@@ -71,24 +62,14 @@ c.content.blocking.hosts.lists = [ \
     'https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/gambling/hosts' \
     ]
 
-# Which cookies to accept. With QtWebEngine, this setting also controls
-# other features with tracking capabilities similar to those of cookies;
-# including IndexedDB, DOM storage, filesystem API, service workers, and
-# AppCache. Note that with QtWebKit, only `all` and `never` are
-# supported as per-domain values. Setting `no-3rdparty` or `no-
-# unknown-3rdparty` per-domain on QtWebKit will have the same effect as
-# `all`.
-c.content.cookies.accept = "no-unknown-3rdparty"
-
-# Some bindings on normal mode
+# keys
 config.bind('xb', 'config-cycle statusbar.show always never')
-config.bind('xt', 'config-cycle tabs.show always never')
+config.bind('xt', 'config-cycle tabs.show multiple never')
 config.bind('xx', 'config-cycle statusbar.show always never;; config-cycle tabs.show always never')
 config.bind(',m', 'hint links spawn mpv {hint-url}')
 config.bind(',M', 'spawn mpv {url}')
 config.bind(',t', 'spawn --userscript translate --text')
-config.bind(',T', 'spawn --userscript translate')
-
+config.bind(',T', 'spawn --userscript translate', "normal")
 config.bind("<Ctrl-h>", "fake-key <Backspace>", "insert")
 config.bind("<Ctrl-a>", "fake-key <Home>", "insert")
 config.bind("<Ctrl-e>", "fake-key <End>", "insert")
@@ -97,31 +78,29 @@ config.bind("<Ctrl-w>", "fake-key <Ctrl-Backspace>", "insert")
 config.bind("<Ctrl-u>", "fake-key <Shift-Home><Delete>", "insert")
 config.bind("<Ctrl-k>", "fake-key <Shift-End><Delete>", "insert")
 
-
-# Setting default page for when opening new tabs/windows
-c.url.start_pages = "file:///home/vido25/.config/qutebrowser/homepage/index.html"
-c.url.default_page = "file:///home/vido25/.config/qutebrowser/homepage/index.html"
-
-# Default search engine, and shortcuts to other. use after :open (-t/-w)
+# url
 c.url.searchengines = {
     'DEFAULT': 'https://duckduckgo.com/?q={}',
 #   'am': 'https://www.amazon.com/s?k={}',
 #   'aw': 'https://wiki.archlinux.org/?search={}',
-#   'ggl': 'https://www.google.com/search?q={}',
+    'ggl': 'https://www.google.com/search?q={}',
 #   'rd': 'https://www.reddit.com/r/{}',
     'wiki': 'https://en.wikipedia.org/wiki/{}',
     'yt': 'https://www.youtube.com/results?search_query={}'
     }
+c.url.start_pages = "file:///home/vido25/.config/qutebrowser/homepage/index.html"
+c.url.default_page = "file:///home/vido25/.config/qutebrowser/homepage/index.html"
 
-# Allow pdf.js to view pdf in browser
-c.content.pdfjs = True
+# per-domain settings
+config.set('content.images', True, 'chrome-devtools://*')
+config.set('content.images', True, 'devtools://*')
+config.set('content.javascript.enabled', True, 'chrome-devtools://*')
+config.set('content.javascript.enabled', True, 'devtools://*')
+config.set('content.javascript.enabled', True, 'chrome://*/*')
+config.set('content.javascript.enabled', True, 'qute://*/*')
 
-# Chars used for hint strings.
-c.hints.chars = "asdfghjklie"
-
-# Font config
+# font
 monospace = "12px 'FiraCode NF'"
-
 c.fonts.hints = "bold 13px 'Ubuntu Mono'"
 c.fonts.default_family = "11px 'FiraCode NF'"
 c.fonts.completion.category = f"bold {monospace}"
@@ -137,357 +116,3 @@ c.fonts.statusbar = monospace
 c.fonts.tabs.selected = monospace
 c.fonts.tabs.unselected = monospace
 
-# Prompt the user for the download location. If set to false,
-# `downloads.location.directory` will be used.
-c.downloads.location.prompt = False
-c.downloads.location.directory = "$HOME/Descargas"
-
-# When to show the autocompletion window.
-# Valid values:
-#   - always: Whenever a completion is available.
-#   - auto: Whenever a completion is requested.
-#   - never: Never.
-c.completion.show = "auto"
-
-# Whether quitting the application requires a confirmation.
-# Valid values:
-#   - always: Always show a confirmation.
-#   - multiple-tabs: Show a confirmation if multiple tabs are opened.
-#   - downloads: Show a confirmation if downloads are running
-#   - never: Never show a confirmation.
-c.confirm_quit = ["downloads"]
-
-# Padding around text for tabs
-c.tabs.padding = {
-    "left": 5,
-    "right": 5,
-    "top": 1,
-    "bottom": 1,
-}
-
-# Enable smooth scrolling for web pages. Note smooth scrolling does not
-# work with the `:scroll-px` command.
-c.scrolling.smooth = True
-
-# The height of the completion, in px or as percentage of the window.
-c.completion.height = "20%"
-
-# Move on to the next part when there's only one possible completion left
-c.completion.quick = False
-
-
-# The format to use for the window title. The following placeholders are
-# defined:
-# `{perc}`: Percentage as a string like `[10%]`.
-# `{perc_raw}`: Raw percentage, e.g. `10`.
-# `{current_title}`: Title of the current web page.
-# `{title_sep}`: The string `" - "` if a title is set, empty otherwise.
-# `{index}`: Index of this tab.
-# `{aligned_index}`: Index of this tab padded with spaces to have the same width.
-# `{id}`: Internal tab ID of this tab.
-# `{scroll_pos}`: Page scroll position.
-# `{host}`: Host of the current web page.
-# `{backend}`: Either `webkit` or `webengine`
-# `{private}`: Indicates when private mode is enabled.
-# `{current_url}`: URL of the current web page.
-# `{protocol}`: Protocol (http/https/...) of the current web page.
-# `{audio}`: Indicator for audio/mute status.
-# c.window.title_format = "{private}{perc}{current_title}{title_sep}qutebrowser"
-c.window.title_format = "{private}{perc}qutebrowser"
-
-# Color themes
-# base16-qutebrowser (https://github.com/theova/base16-qutebrowser)
-base00 = "#151515"
-base01 = "#202020"
-base02 = "#303030"
-base03 = "#505050"
-base04 = "#B0B0B0"
-base05 = "#D0D0D0"
-base06 = "#E0E0E0"
-base07 = "#F5F5F5"
-base08 = "#AC4142"
-base09 = "#D28445"
-base0A = "#F4BF75"
-base0B = "#90A959"
-base0C = "#75B5AA"
-base0D = "#6A9FB5"
-base0E = "#AA759F"
-base0F = "#8F5536"
-
-# Text color of the completion widget. May be a single color to use for
-# all columns or a list of three colors, one for each column.
-c.colors.completion.fg = base05
-
-# Background color of the completion widget for odd rows.
-c.colors.completion.odd.bg = base00
-
-# Background color of the completion widget for even rows.
-c.colors.completion.even.bg = base00
-
-# Foreground color of completion widget category headers.
-c.colors.completion.category.fg = base0D
-
-# Background color of the completion widget category headers.
-c.colors.completion.category.bg = base00
-
-# Top border color of the completion widget category headers.
-c.colors.completion.category.border.top = base00
-
-# Bottom border color of the completion widget category headers.
-c.colors.completion.category.border.bottom = base00
-
-# Foreground color of the selected completion item.
-c.colors.completion.item.selected.fg = base05
-
-# Background color of the selected completion item.
-c.colors.completion.item.selected.bg = base02
-
-# Top border color of the selected completion item.
-c.colors.completion.item.selected.border.top = base02
-
-# Bottom border color of the selected completion item.
-c.colors.completion.item.selected.border.bottom = base02
-
-# Foreground color of the matched text in the selected completion item.
-c.colors.completion.item.selected.match.fg = base05
-
-# Foreground color of the matched text in the completion.
-c.colors.completion.match.fg = base09
-
-# Color of the scrollbar handle in the completion view.
-c.colors.completion.scrollbar.fg = base05
-
-# Color of the scrollbar in the completion view.
-c.colors.completion.scrollbar.bg = base00
-
-# Background color of disabled items in the context menu.
-c.colors.contextmenu.disabled.bg = base01
-
-# Foreground color of disabled items in the context menu.
-c.colors.contextmenu.disabled.fg = base04
-
-# Background color of the context menu. If set to null, the Qt default is used.
-c.colors.contextmenu.menu.bg = base00
-
-# Foreground color of the context menu. If set to null, the Qt default is used.
-c.colors.contextmenu.menu.fg =  base05
-
-# Background color of the context menu’s selected item. If set to null, the Qt default is used.
-c.colors.contextmenu.selected.bg = base02
-
-#Foreground color of the context menu’s selected item. If set to null, the Qt default is used.
-c.colors.contextmenu.selected.fg = base05
-
-# Background color for the download bar.
-c.colors.downloads.bar.bg = base00
-
-# Color gradient start for download text.
-c.colors.downloads.start.fg = base00
-
-# Color gradient start for download backgrounds.
-c.colors.downloads.start.bg = base0D
-
-# Color gradient end for download text.
-c.colors.downloads.stop.fg = base00
-
-# Color gradient stop for download backgrounds.
-c.colors.downloads.stop.bg = base0C
-
-# Foreground color for downloads with errors.
-c.colors.downloads.error.fg = base08
-
-# Font color for hints.
-c.colors.hints.fg = base00
-
-# Background color for hints. Note that you can use a `rgba(...)` value
-# for transparency.
-c.colors.hints.bg = base0A
-
-# Font color for the matched part of hints.
-c.colors.hints.match.fg = base05
-
-# Text color for the keyhint widget.
-c.colors.keyhint.fg = base05
-
-# Highlight color for keys to complete the current keychain.
-c.colors.keyhint.suffix.fg = base05
-
-# Background color of the keyhint widget.
-c.colors.keyhint.bg = base00
-
-# Foreground color of an error message.
-c.colors.messages.error.fg = base00
-
-# Background color of an error message.
-c.colors.messages.error.bg = base08
-
-# Border color of an error message.
-c.colors.messages.error.border = base08
-
-# Foreground color of a warning message.
-c.colors.messages.warning.fg = base00
-
-# Background color of a warning message.
-c.colors.messages.warning.bg = base0E
-
-# Border color of a warning message.
-c.colors.messages.warning.border = base0E
-
-# Foreground color of an info message.
-c.colors.messages.info.fg = base05
-
-# Background color of an info message.
-c.colors.messages.info.bg = base00
-
-# Border color of an info message.
-c.colors.messages.info.border = base00
-
-# Foreground color for prompts.
-c.colors.prompts.fg = base05
-
-# Border used around UI elements in prompts.
-c.colors.prompts.border = base00
-
-# Background color for prompts.
-c.colors.prompts.bg = base00
-
-# Background color for the selected item in filename prompts.
-c.colors.prompts.selected.bg = base02
-
-# Foreground color for the selected item in filename prompts.
-#c.colors.prompts.selected.fg = base05
-
-# Foreground color of the statusbar.
-c.colors.statusbar.normal.fg = base05
-
-# Background color of the statusbar.
-c.colors.statusbar.normal.bg = base00
-
-# Foreground color of the statusbar in insert mode.
-c.colors.statusbar.insert.fg = base0C
-
-# Background color of the statusbar in insert mode.
-c.colors.statusbar.insert.bg = base00
-
-# Foreground color of the statusbar in passthrough mode.
-c.colors.statusbar.passthrough.fg = base0A
-
-# Background color of the statusbar in passthrough mode.
-c.colors.statusbar.passthrough.bg = base00
-
-# Foreground color of the statusbar in private browsing mode.
-c.colors.statusbar.private.fg = base0E
-
-# Background color of the statusbar in private browsing mode.
-c.colors.statusbar.private.bg = base00
-
-# Foreground color of the statusbar in command mode.
-c.colors.statusbar.command.fg = base04
-
-# Background color of the statusbar in command mode.
-c.colors.statusbar.command.bg = base01
-
-# Foreground color of the statusbar in private browsing + command mode.
-c.colors.statusbar.command.private.fg = base0E
-
-# Background color of the statusbar in private browsing + command mode.
-c.colors.statusbar.command.private.bg = base01
-
-# Foreground color of the statusbar in caret mode.
-c.colors.statusbar.caret.fg = base0D
-
-# Background color of the statusbar in caret mode.
-c.colors.statusbar.caret.bg = base00
-
-# Foreground color of the statusbar in caret mode with a selection.
-c.colors.statusbar.caret.selection.fg = base0D
-
-# Background color of the statusbar in caret mode with a selection.
-c.colors.statusbar.caret.selection.bg = base00
-
-# Background color of the progress bar.
-c.colors.statusbar.progress.bg = base0D
-
-# Default foreground color of the URL in the statusbar.
-c.colors.statusbar.url.fg = base05
-
-# Foreground color of the URL in the statusbar on error.
-c.colors.statusbar.url.error.fg = base08
-
-# Foreground color of the URL in the statusbar for hovered links.
-c.colors.statusbar.url.hover.fg = base09
-
-# Foreground color of the URL in the statusbar on successful load
-# (http).
-c.colors.statusbar.url.success.http.fg = base0B
-
-# Foreground color of the URL in the statusbar on successful load
-# (https).
-c.colors.statusbar.url.success.https.fg = base0B
-
-# Foreground color of the URL in the statusbar when there's a warning.
-c.colors.statusbar.url.warn.fg = base0E
-
-# Background color of the tab bar.
-c.colors.tabs.bar.bg = base00
-
-# Color gradient start for the tab indicator.
-c.colors.tabs.indicator.start = base0D
-
-# Color gradient end for the tab indicator.
-c.colors.tabs.indicator.stop = base0C
-
-# Color for the tab indicator on errors.
-c.colors.tabs.indicator.error = base08
-
-# Foreground color of unselected odd tabs.
-c.colors.tabs.odd.fg = base05
-
-# Background color of unselected odd tabs.
-c.colors.tabs.odd.bg = base00
-
-# Foreground color of unselected even tabs.
-c.colors.tabs.even.fg = base05
-
-# Background color of unselected even tabs.
-c.colors.tabs.even.bg = base00
-
-# Background color of pinned unselected even tabs.
-c.colors.tabs.pinned.even.bg = base0B
-
-# Foreground color of pinned unselected even tabs.
-c.colors.tabs.pinned.even.fg = base00
-
-# Background color of pinned unselected odd tabs.
-c.colors.tabs.pinned.odd.bg = base0B
-
-# Foreground color of pinned unselected odd tabs.
-c.colors.tabs.pinned.odd.fg = base00
-
-# Background color of pinned selected even tabs.
-c.colors.tabs.pinned.selected.even.bg = base02
-
-# Foreground color of pinned selected even tabs.
-c.colors.tabs.pinned.selected.even.fg = base05
-
-# Background color of pinned selected odd tabs.
-c.colors.tabs.pinned.selected.odd.bg = base02
-
-# Foreground color of pinned selected odd tabs.
-c.colors.tabs.pinned.selected.odd.fg = base05
-
-# Foreground color of selected odd tabs.
-c.colors.tabs.selected.odd.fg = base05
-
-# Background color of selected odd tabs.
-c.colors.tabs.selected.odd.bg = base02
-
-# Foreground color of selected even tabs.
-c.colors.tabs.selected.even.fg = base05
-
-# Background color of selected even tabs.
-c.colors.tabs.selected.even.bg = base02
-
-# Background color for webpages if unset (or empty to use the theme's
-# color).
-c.colors.webpage.bg = base00
