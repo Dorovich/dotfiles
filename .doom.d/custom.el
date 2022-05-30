7a2bb40b9a2203d2ec49b7e024c174ca4037bcee
Dorovich <silverdovi25@gmail.com>
2022-05-30 19:14:55 +0200

    doom

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -2,7 +2,7 @@
  '(org-agenda-files '("~/Documentos/org/agenda.org")))
 (custom-set-faces)
 
-(setq fancy-splash-image "/media/vido25/Almacenamiento/c o o l/hmmm_cut.png")
+(setq fancy-splash-image "~/.doom.d/hmmm_cut.png")
 
 (evil-define-key 'normal 'global (kbd "ñ") 'evil-ex)
 (evil-define-key 'normal 'global (kbd "U") 'evil-redo)
