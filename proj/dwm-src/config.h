03f5903786b3ac7d573ff126c8597f8ed2a05e62
Dorovich <silverdovi25@gmail.com>
2022-06-09 22:42:13 +0200

    dwm fullscreen patch

--- proj/dwm-src/config.h
+++ proj/dwm-src/config.h
@@ -93,6 +93,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY|SHIFT,                 XK_space,  setlayout,      {0} },
 	{ MODKEY,                       XK_space,  togglefloating, {0} },
+	{ MODKEY|SHIFT,                 XK_f,      togglefullscr,  {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|SHIFT,                 XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
