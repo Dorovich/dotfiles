85c2393abe7c8eac119502df26d4d7ef60bcf135
Dorovich <silverdovi25@gmail.com>
2022-06-10 16:28:01 +0200

    slock

--- proj/slock-src/config.h
+++ proj/slock-src/config.h
@@ -4,7 +4,7 @@ static const char *group = "nogroup";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
+	[INPUT] =  "#394338",   /* during input */
 	[FAILED] = "#CC3333",   /* wrong password */
 };
 
