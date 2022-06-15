c5bed725be03f214250bc048c3711d31a87ee9f5
Dorovich <silverdovi25@gmail.com>
2022-06-15 20:30:17 +0200

    vimb

--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -42,7 +42,6 @@ static const Rule rules[] = {
 	/* class             instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",            NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",         NULL,       NULL,       0,            0,           -1 },
-	{ "Lista de amigos", NULL,       NULL,       0,            1,           -1 },
 	{ "eog",             NULL,       NULL,       0,            1,           -1 },
 };
 
