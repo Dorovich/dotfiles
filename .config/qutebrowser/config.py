a79c5d4871b5009bf0bb0837daede6ef0323d872
Dorovich <silverdovi25@gmail.com>
2022-07-20 00:06:39 +0200

    qutebrowser

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,24 +1,19 @@
-# General {{{
+#             __      __
+#  ___ ___ __/ /____ / /  _______ _    _____ ___ ____
+# / _ `/ // / __/ -_) _ \/ __/ _ \ |/|/ (_-</ -_) __/
+# \_, /\_,_/\__/\__/_.__/_/  \___/__,__/___/\__/_/
+#  /_
+#
+# qute://help/index.html
+
 config.load_autoconfig(False)
-c.downloads.location.prompt = False
-c.downloads.location.directory = "$HOME/Descargas"
-c.confirm_quit = ["downloads"]
-c.content.autoplay = False
-c.content.pdfjs = True
-c.hints.chars = "asdfghjklie"
-c.tabs.show = "multiple" # 'switching' alternativamente
-c.statusbar.show = "never" # 'in-mode' alternativamente
-c.tabs.last_close = "ignore"  # "close" alternativamente
-c.content.default_encoding = "utf-8"
-c.content.fullscreen.overlay_timeout = 500
-c.input.partial_timeout = 500
-c.editor.command = ['st', '-e', 'nvim', '{}']
-c.tabs.select_on_remove = "prev"
-c.tabs.background = False
-# }}}
+c.url.default_page = "dorovich.github.io/b"
+c.url.start_pages = ["dorovich.github.io/b"]
 
 # UI {{{
 config.source("colors.py")
+c.hints.chars = "asdfghjklie"
+c.statusbar.show = "never" # 'in-mode' alternativamente
 c.colors.webpage.preferred_color_scheme = "dark"
 c.colors.webpage.darkmode.enabled = False
 c.colors.webpage.darkmode.algorithm = 'lightness-cielab'
@@ -30,6 +25,11 @@ c.scrolling.smooth = True
 c.completion.height = "20%"
 c.completion.quick = False
 c.window.title_format = "{private}{perc}qutebrowser"
+c.content.fullscreen.overlay_timeout = 500
+c.tabs.last_close = "ignore"  # "close" alternativamente
+c.tabs.show = "multiple" # 'switching' alternativamente
+c.tabs.select_on_remove = "prev"
+c.tabs.background = True
 c.tabs.padding = {
     "left": 5,
     "right": 5,
@@ -38,7 +38,82 @@ c.tabs.padding = {
 }
 # }}}
 
-# Privacy {{{
+# Mappings {{{
+c.input.partial_timeout = 500
+
+config.bind('ñ', 'clear-messages ;; set-cmd-text :')
+config.bind('o', 'clear-messages ;; set-cmd-text -s :open')
+config.bind('O', 'clear-messages ;; set-cmd-text :open {url}')
+config.bind('t', 'clear-messages ;; set-cmd-text -s :open -t')
+config.bind('T', 'clear-messages ;; set-cmd-text :open -t {url}')
+config.bind('y', 'yank selection')
+config.bind('Y', 'yank pretty-url')
+config.bind('p', 'open {clipboard}')
+config.bind('P', 'open -t {clipboard}')
+config.bind('c', 'clear-messages')
+config.bind('jk', 'mode-leave', 'command')
+
+config.bind('xb', 'config-cycle statusbar.show never in-mode')
+config.bind('xt', 'config-cycle tabs.show multiple switching')
+config.bind('xx', 'config-cycle statusbar.show in-mode never;; config-cycle tabs.show multiple switching')
+
+config.bind(',m', 'hint links spawn mpv {hint-url}')
+config.bind(',M', 'spawn mpv {url}')
+config.bind(',t', 'spawn --userscript translate --text')
+config.bind(',T', 'spawn --userscript translate')
+config.bind(',d', 'download-open')
+
+config.bind('<Ctrl-j>', 'tab-move +')
+config.bind('<Ctrl-k>', 'tab-move -')
+config.bind('<Ctrl-i>', 'forward')
+config.bind('<Ctrl-o>', 'back')
+config.bind('<Ctrl-e>', 'config-edit')
+config.unbind('<ctrl+tab>')
+config.bind('<ctrl+tab>', 'tab-next')
+config.bind('<ctrl+shift+tab>', 'tab-prev')
+config.bind('<ctrl+shift+p>', 'open -p')
+config.bind('<Ctrl-p>', 'spawn --userscript password_fill')
+config.bind('<Ctrl-p>', 'spawn --userscript password_fill', 'insert')
+
+config.bind('<Ctrl-h>', 'fake-key <Backspace>', 'insert')
+config.bind('<Ctrl-a>', 'fake-key <Home>', 'insert')
+config.bind('<Ctrl-e>', 'fake-key <End>', 'insert')
+config.bind('<Ctrl-b>', 'fake-key <Delete>', 'insert')
+config.bind('<Ctrl-w>', 'fake-key <Ctrl-Backspace>', 'insert')
+config.bind('<Ctrl-u>', 'fake-key <Shift-Home><Delete>', 'insert')
+config.bind('<Ctrl-k>', 'fake-key <Shift-End><Delete>', 'insert')
+config.bind('<Ctrl-d>', 'fake-key -g <Esc>', 'insert')
+# }}}
+
+# Shortcuts {{{
+c.url.searchengines = {
+    'DEFAULT': 'https://duckduckgo.com/?q={}',
+    'ddg': 'https://duckduckgo.com/?q={}',
+    'ggl': 'https://encrypted.google.com/search?q={}',
+    'sx': 'https://searx.tiekoetter.net/search?q={}',
+    'yt': 'https://www.youtube.com/results?search_query={}',
+    'aw': 'https://wiki.archlinux.org/?search={}',
+    'rd': 'https://www.reddit.com/r/{}',
+    'gh': 'https://github.com/search?q={}',
+    'wa': 'https://www.wolframalpha.com/input?i={}',
+    'map': 'https://maps.google.com/maps?saddr={}',
+    'trad': 'https://translate.google.com/?hl=es&sl=auto&tl=es&text={}&op=translate',
+    'wiki': 'https://en.wikipedia.org/wiki/{}',
+    'img': 'https://duckduckgo.com/?q={}&t=h_&iax=images&ia=images',
+    'mal': 'https://myanimelist.net/search/all?q={}',
+    'inv': 'https://yewtu.be/search?q={}',
+    'fc': 'https://4chan.org/{}/'
+    }
+# }}}
+
+# Web content {{{
+c.editor.command = ['st', '-e', 'nvim', '{}']
+c.downloads.location.directory = "$HOME/Descargas"
+c.downloads.location.prompt = False
+c.confirm_quit = ["downloads"]
+c.content.autoplay = False
+c.content.pdfjs = True
+c.content.default_encoding = "utf-8"
 c.content.webrtc_ip_handling_policy = "default-public-interface-only"
 c.content.geolocation = False
 c.content.headers.user_agent = "Mozilla/5.0 ({os_info}; rv:102.0) Gecko/20100101 Firefox/102.0"
@@ -75,74 +150,26 @@ c.content.blocking.hosts.lists = [ \
     ]
 # }}}
 
-# Keys {{{
-config.bind('ñ', 'clear-messages ;; set-cmd-text :')
-config.bind('o', 'clear-messages ;; set-cmd-text -s :open')
-config.bind('O', 'clear-messages ;; set-cmd-text :open {url}')
-config.bind('t', 'clear-messages ;; set-cmd-text -s :open -t')
-config.bind('T', 'clear-messages ;; set-cmd-text :open -t {url}')
-config.bind('y', 'yank selection')
-config.bind('Y', 'yank pretty-url')
-config.bind('p', 'open {clipboard}')
-config.bind('P', 'open -t {clipboard}')
-config.bind('c', 'clear-messages')
-config.bind('jk', 'mode-leave', 'command')
-config.unbind('<ctrl+tab>')
-config.bind('<ctrl+tab>', 'tab-next')
-config.bind('<ctrl+shift+tab>', 'tab-prev')
-config.bind('<ctrl+shift+p>', 'open -p')
-
-config.bind('xb', 'config-cycle statusbar.show never in-mode')
-config.bind('xt', 'config-cycle tabs.show multiple switching')
-config.bind('xx', 'config-cycle statusbar.show in-mode never;; config-cycle tabs.show multiple switching')
-
-config.bind(',m', 'hint links spawn mpv {hint-url}')
-config.bind(',M', 'spawn mpv {url}')
-config.bind(',t', 'spawn --userscript translate --text')
-config.bind(',T', 'spawn --userscript translate')
-config.bind(',d', 'download-open')
-
-config.bind('<Ctrl-j>', 'tab-move +')
-config.bind('<Ctrl-k>', 'tab-move -')
-config.bind('<Ctrl-i>', 'forward')
-config.bind('<Ctrl-o>', 'back')
-
-config.bind('<Ctrl-p>', 'spawn --userscript password_fill')
-config.bind('<Ctrl-p>', 'spawn --userscript password_fill', 'insert')
-
-config.bind('<Ctrl-h>', 'fake-key <Backspace>', 'insert')
-config.bind('<Ctrl-a>', 'fake-key <Home>', 'insert')
-config.bind('<Ctrl-e>', 'fake-key <End>', 'insert')
-config.bind('<Ctrl-d>', 'fake-key <Delete>', 'insert')
-config.bind('<Ctrl-w>', 'fake-key <Ctrl-Backspace>', 'insert')
-config.bind('<Ctrl-u>', 'fake-key <Shift-Home><Delete>', 'insert')
-config.bind('<Ctrl-k>', 'fake-key <Shift-End><Delete>', 'insert')
+# Fonts {{{
+monospace = "16px 'mononoki NF'"
+c.fonts.hints = f"bold {monospace}"
+c.fonts.default_family = monospace
+c.fonts.completion.category = f"bold {monospace}"
+c.fonts.completion.entry = monospace
+c.fonts.debug_console = monospace
+c.fonts.downloads = monospace
+c.fonts.keyhint = monospace
+c.fonts.messages.error = monospace
+c.fonts.messages.info = monospace
+c.fonts.messages.warning = monospace
+c.fonts.prompts = monospace
+c.fonts.statusbar = monospace
+#c.fonts.tabs.selected = f"bold {monospace}"
+c.fonts.tabs.selected = monospace
+c.fonts.tabs.unselected = monospace
 # }}}
 
-# URL {{{
-c.url.searchengines = {
-    'DEFAULT': 'https://duckduckgo.com/?q={}',
-    'ddg': 'https://duckduckgo.com/?q={}',
-    'ggl': 'https://encrypted.google.com/search?q={}',
-    'sx': 'https://searx.tiekoetter.net/search?q={}',
-    'yt': 'https://www.youtube.com/results?search_query={}',
-    'aw': 'https://wiki.archlinux.org/?search={}',
-    'rd': 'https://www.reddit.com/r/{}',
-    'gh': 'https://github.com/search?q={}',
-    'wa': 'https://www.wolframalpha.com/input?i={}',
-    'map': 'https://maps.google.com/maps?saddr={}',
-    'trad': 'https://translate.google.com/?hl=es&sl=auto&tl=es&text={}&op=translate',
-    'wiki': 'https://en.wikipedia.org/wiki/{}',
-    'img': 'https://duckduckgo.com/?q={}&t=h_&iax=images&ia=images',
-    'mal': 'https://myanimelist.net/search/all?q={}',
-    'inv': 'https://yewtu.be/search?q={}',
-    'fc': 'https://4chan.org/{}/'
-    }
-c.url.default_page = "dorovich.github.io/b"
-c.url.start_pages = ["dorovich.github.io/b"]
-# }}}
-
-# Per-domain Settings {{{
+# Per-domain {{{
 config.set('content.images', True, 'chrome-devtools://*')
 config.set('content.images', True, 'devtools://*')
 config.set('content.javascript.enabled', True, 'chrome-devtools://*')
@@ -172,23 +199,4 @@ config.set('content.javascript.enabled', True, 'qute://*/*')
 #config.set('content.cookies.accept', 'no-3rdparty', 'https://www.twitch.tv/*')
 #config.set('content.cookies.accept', 'no-3rdparty', 'https://es.wallapop.com/*')
 #config.set('content.cookies.accept', 'no-3rdparty', 'https://5e.tools/*')
-# }}}
-
-# Font {{{
-monospace = "16px 'mononoki NF'"
-c.fonts.hints = f"bold {monospace}"
-c.fonts.default_family = monospace
-c.fonts.completion.category = f"bold {monospace}"
-c.fonts.completion.entry = monospace
-c.fonts.debug_console = monospace
-c.fonts.downloads = monospace
-c.fonts.keyhint = monospace
-c.fonts.messages.error = monospace
-c.fonts.messages.info = monospace
-c.fonts.messages.warning = monospace
-c.fonts.prompts = monospace
-c.fonts.statusbar = monospace
-#c.fonts.tabs.selected = f"bold {monospace}"
-c.fonts.tabs.selected = monospace
-c.fonts.tabs.unselected = monospace
 # }}}
