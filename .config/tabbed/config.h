05c31cf658a338dc9b59609461d10a2d31fdea08
Dorovich <silverdovi25@gmail.com>
2022-07-23 20:22:05 +0200

    vimb, tabbed

--- .config/tabbed/config.h
+++ .config/tabbed/config.h
@@ -37,45 +37,46 @@ static Bool npisrelative  = False;
 /* CONTROL+SHIFT is used extensively in the surf browser. */
 /* Use CONTROL+ALT instead! */
 
-#define MODKEY ControlMask
+#define ALT Mod1Mask
+#define CONTROL ControlMask
 static Key keys[] = {
 	/* modifier             key        function     argument */
-	{ MODKEY|Mod1Mask,      XK_Return, focusonce,   { 0 } },
-	{ MODKEY|Mod1Mask,      XK_Return, spawn,       { 0 } },
+	{ ALT|CONTROL,          XK_Return, focusonce,   { 0 } },
+	{ ALT|CONTROL,          XK_Return, spawn,       { 0 } },
 
-	{ MODKEY|Mod1Mask,      XK_l,      rotate,      { .i = +1 } },
-	{ MODKEY|Mod1Mask,      XK_h,      rotate,      { .i = -1 } },
-	{ MODKEY|Mod1Mask,      XK_j,      movetab,     { .i = -1 } },
-	{ MODKEY|Mod1Mask,      XK_k,      movetab,     { .i = +1 } },
-	{ MODKEY,               XK_Tab,    rotate,      { .i = 0 } },
+	{ ALT|CONTROL,          XK_l,      rotate,      { .i = +1 } },
+	{ ALT|CONTROL,          XK_h,      rotate,      { .i = -1 } },
+	{ ALT|CONTROL,          XK_j,      movetab,     { .i = -1 } },
+	{ ALT|CONTROL,          XK_k,      movetab,     { .i = +1 } },
+	{ ALT,                  XK_Tab,    rotate,      { .i = 0 } },
 
-	{ MODKEY,               XK_grave,  spawn,       SETPROP("_TABBED_SELECT_TAB") },
-	{ MODKEY,               XK_1,      move,        { .i = 0 } },
-	{ MODKEY,               XK_2,      move,        { .i = 1 } },
-	{ MODKEY,               XK_3,      move,        { .i = 2 } },
-	{ MODKEY,               XK_4,      move,        { .i = 3 } },
-	{ MODKEY,               XK_5,      move,        { .i = 4 } },
-	{ MODKEY,               XK_6,      move,        { .i = 5 } },
-	{ MODKEY,               XK_7,      move,        { .i = 6 } },
-	{ MODKEY,               XK_8,      move,        { .i = 7 } },
-	{ MODKEY,               XK_9,      move,        { .i = 8 } },
-	{ MODKEY,               XK_0,      move,        { .i = 9 } },
+	{ ALT,                  XK_grave,  spawn,       SETPROP("_TABBED_SELECT_TAB") },
+	{ ALT,                  XK_1,      move,        { .i = 0 } },
+	{ ALT,                  XK_2,      move,        { .i = 1 } },
+	{ ALT,                  XK_3,      move,        { .i = 2 } },
+	{ ALT,                  XK_4,      move,        { .i = 3 } },
+	{ ALT,                  XK_5,      move,        { .i = 4 } },
+	{ ALT,                  XK_6,      move,        { .i = 5 } },
+	{ ALT,                  XK_7,      move,        { .i = 6 } },
+	{ ALT,                  XK_8,      move,        { .i = 7 } },
+	{ ALT,                  XK_9,      move,        { .i = 8 } },
+	{ ALT,                  XK_0,      move,        { .i = 9 } },
 
-	{ MODKEY|Mod1Mask,      XK_q,      killclient,  { 0 } },
+	{ ALT|CONTROL,          XK_q,      killclient,  { 0 } },
 
-	{ MODKEY,               XK_u,      focusurgent, { 0 } },
-	{ MODKEY|Mod1Mask,      XK_u,      toggle,      { .v = (void*) &urgentswitch } },
+	{ ALT,                  XK_u,      focusurgent, { 0 } },
+	{ ALT|CONTROL,          XK_u,      toggle,      { .v = (void*) &urgentswitch } },
 
 	{ 0,                    XK_F11,    fullscreen,  { 0 } },
 
-	{ MODKEY,               XK_Alt_L,     showbar,    { .i = 1 } },
-	{ Mod1Mask,             XK_Control_L, showbar,  { .i = 1 } },
-	{ MODKEY,               XK_period,    showbar,     { .i = 1 } },
-	{ MODKEY,               XK_comma,     showbar,      { .i = 0 } },
+	{ CONTROL,              XK_Alt_L,     showbar,    { .i = 1 } },
+	{ ALT,                  XK_Control_L, showbar,    { .i = 1 } },
+	{ ALT,                  XK_period,    showbar,    { .i = 1 } },
+	{ ALT,                  XK_comma,     showbar,    { .i = 0 } },
 };
 
 static Key keyreleases[] = {
 	/* modifier             key           function  argument */
-	{ MODKEY|Mod1Mask,      XK_Alt_L,     showbar,  { .i = 0 } },
-	{ MODKEY|Mod1Mask,      XK_Control_L, showbar,  { .i = 0 } },
+	{ ALT|CONTROL,          XK_Alt_L,     showbar,  { .i = 0 } },
+	{ ALT|CONTROL,          XK_Control_L, showbar,  { .i = 0 } },
 };
