84182741a50ed4f4dda25a4ba7658c76cde44243
Dorovich <silverdovi25@gmail.com>
2022-10-03 20:24:54 +0200

    herbst

--- .config/herbstluftwm/scratchpad.sh
+++ .config/herbstluftwm/scratchpad.sh
@@ -2,7 +2,7 @@
 
 scratchpad=/tmp/herbstluftwm:scratchpad
 
-if [[ $(xdotool search --onlyvisible --name 'scratchpad') ]]; then
+if [[ $(xdotool search --onlyvisible --class 'scratchpad') ]]; then
     if [ $(herbstclient list_monitors | grep '[FOCUS]' | cut -d'"' -f2) = $(herbstclient attr clients.$(cat $scratchpad) | grep 's - - tag' | awk '{ print $6 }' | cut -d'"' -f2) ]; then 
         xdotool search --onlyvisible --class 'scratchpad' windowunmap && exit 
     fi 
@@ -10,11 +10,11 @@ fi
 
 if [ -f "${scratchpad}" ]; then
     if [ -n $(herbstclient chain , bring $(cat "${scratchpad}") , jumpto $(cat "${scratchpad}")) ]; then
-      xdotool search --class 'scratchpad' windowmap && exit
+        xdotool search --class 'scratchpad' windowmap && exit
     fi
 fi
 
-if [ -n $(xdotool search --name 'scratchpad' windowmap) ]; then
+if [ -n $(xdotool search --class 'scratchpad' windowmap) ]; then
     st -n 'scratchpad' -t 'scratchpad' -c 'scratchpad'
     xdotool search --sync --onlyvisible --class 'scratchpad'
     herbstclient attr clients.focus.winid > "${scratchpad}"
