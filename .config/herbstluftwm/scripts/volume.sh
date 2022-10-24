60281920a9b7dcd43a730ed054938744f7bb2bec
Dorovich <silverdovi25@gmail.com>
2022-10-24 15:08:12 +0200

    aaa

--- .config/herbstluftwm/scripts/volume.sh
+++ .config/herbstluftwm/scripts/volume.sh
@@ -3,8 +3,12 @@
 case $1 in
     1)      # mpc volume up
         mpc volume +5
+        pkill herbe
+        herbe "mpc $(mpc volume)"
         ;;
     2)      # mpc volume down
         mpc volume -5
+        pkill herbe
+        herbe "mpc $(mpc volume)"
         ;;
 esac
