26ad48545952089fccab10862473ac257c4a85d5
Dorovich <silverdovi25@gmail.com>
2022-11-08 17:50:26 +0100

    emacs

---
+++ .doom.d/custom.el
@@ -0,0 +1,50 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#f7f3ee" "#955f5f" "#81895d" "#957f5f" "#7382a0" "#9c739c" "#5f8c7d" "#605a52"])
+ '(custom-safe-themes
+   '("be84a2e5c70f991051d4aaf0f049fa11c172e5d784727e0b525565bb1533ec78" "6945dadc749ac5cbd47012cad836f92aea9ebec9f504d32fe89a956260773ca4" "f458b92de1f6cf0bdda6bce23433877e94816c3364b821eb4ea9852112f5d7dc" "f64189544da6f16bab285747d04a92bd57c7e7813d8c24c30f382f087d460a33" "2078837f21ac3b0cc84167306fa1058e3199bbd12b6d5b56e3777a4125ff6851" "c865644bfc16c7a43e847828139b74d1117a6077a845d16e71da38c8413a5aaa" "8d8207a39e18e2cc95ebddf62f841442d36fcba01a2a9451773d4ed30b632443" "2853dd90f0d49439ebd582a8cbb82b9b3c2a02593483341b257f88add195ad76" "636b135e4b7c86ac41375da39ade929e2bd6439de8901f53f88fde7dd5ac3561" "bf948e3f55a8cd1f420373410911d0a50be5a04a8886cabe8d8e471ad8fdba8e" "ae426fc51c58ade49774264c17e666ea7f681d8cae62570630539be3d06fd964" "dc8285f7f4d86c0aebf1ea4b448842a6868553eded6f71d1de52f3dcbc960039" "0c08a5c3c2a72e3ca806a29302ef942335292a80c2934c1123e8c732bb2ddd77" "512ce140ea9c1521ccaceaa0e73e2487e2d3826cc9d287275550b47c04072bc4" "3fe1ebb870cc8a28e69763dde7b08c0f6b7e71cc310ffc3394622e5df6e4f0da" "683b3fe1689da78a4e64d3ddfce90f2c19eb2d8ab1bab1738a63d8263119c3f4" "4fda8201465755b403a33e385cf0f75eeec31ca8893199266a6aeccb4adedfa4" "680f62b751481cc5b5b44aeab824e5683cf13792c006aeba1c25ce2d89826426" "00cec71d41047ebabeb310a325c365d5bc4b7fab0a681a2a108d32fb161b4006" "f053f92735d6d238461da8512b9c071a5ce3b9d972501f7a5e6682a90bf29725" "5b9a45080feaedc7820894ebbfe4f8251e13b66654ac4394cb416fef9fdca789" "da75eceab6bea9298e04ce5b4b07349f8c02da305734f7c0c8c6af7b5eaa9738" "1cae4424345f7fe5225724301ef1a793e610ae5a4e23c023076dc334a9eb940a" "49acd691c89118c0768c4fb9a333af33e3d2dca48e6f79787478757071d64e68" "631c52620e2953e744f2b56d102eae503017047fb43d65ce028e88ef5846ea3b" "545ab1a535c913c9214fe5b883046f02982c508815612234140240c129682a68" "89d9dc6f4e9a024737fb8840259c5dd0a140fd440f5ed17b596be43a05d62e67" "2dd4951e967990396142ec54d376cced3f135810b2b69920e77103e0bcedfba9" "ddffe74bc4bf2c332c2c3f67f1b8141ee1de8fd6b7be103ade50abb97fe70f0c" "e1f4f0158cd5a01a9d96f1f7cdcca8d6724d7d33267623cc433fe1c196848554" "016f665c0dd5f76f8404124482a0b13a573d17e92ff4eb36a66b409f4d1da410" "60ada0ff6b91687f1a04cc17ad04119e59a7542644c7c59fc135909499400ab8" "8b6506330d63e7bc5fb940e7c177a010842ecdda6e1d1941ac5a81b13191020e" "ce4234c32262924c1d2f43e6b61312634938777071f1129c7cde3ebd4a3028da" "adaf421037f4ae6725aa9f5654a2ed49e2cd2765f71e19a7d26a454491b486eb" "991ca4dbb23cab4f45c1463c187ac80de9e6a718edc8640003892a2523cb6259" "9d29a302302cce971d988eb51bd17c1d2be6cd68305710446f658958c0640f68" "7a424478cb77a96af2c0f50cfb4e2a88647b3ccca225f8c650ed45b7f50d9525" "570263442ce6735821600ec74a9b032bc5512ed4539faf61168f2fdf747e0668" "251ed7ecd97af314cd77b07359a09da12dcd97be35e3ab761d4a92d8d8cf9a71" "5f128efd37c6a87cd4ad8e8b7f2afaba425425524a68133ac0efd87291d05874" "b9761a2e568bee658e0ff723dd620d844172943eb5ec4053e2b199c59e0bcc22" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "1aa4243143f6c9f2a51ff173221f4fd23a1719f4194df6cef8878e75d349613d" "7e377879cbd60c66b88e51fad480b3ab18d60847f31c435f15f5df18bdb18184" "02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" "4ff1c4d05adad3de88da16bd2e857f8374f26f9063b2d77d38d14686e3868d8d" "5586a5db9dadef93b6b6e72720205a4fa92fd60e4ccfd3a5fa389782eab2371b" default))
+ '(exwm-floating-border-color "#c6bdb2")
+ '(fci-rule-color "#605a52")
+ '(highlight-tail-colors ((("#ebe8df") . 0) (("#e7e8e2") . 20)))
+ '(ispell-dictionary nil)
+ '(jdee-db-active-breakpoint-face-colors (cons "#f1ece4" "#7382a0"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#f1ece4" "#81895d"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#f1ece4" "#b9a992"))
+ '(objed-cursor-color "#955f5f")
+ '(pdf-view-midnight-colors (cons "#605a52" "#f7f3ee"))
+ '(rustic-ansi-faces
+   ["#f7f3ee" "#955f5f" "#81895d" "#957f5f" "#7382a0" "#9c739c" "#5f8c7d" "#605a52"])
+ '(vc-annotate-background "#f7f3ee")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#81895d")
+    (cons 40 "#87855d")
+    (cons 60 "#8e825e")
+    (cons 80 "#957f5f")
+    (cons 100 "#957f5f")
+    (cons 120 "#957f5f")
+    (cons 140 "#957f5f")
+    (cons 160 "#977b73")
+    (cons 180 "#997787")
+    (cons 200 "#9c739c")
+    (cons 220 "#996c87")
+    (cons 240 "#976573")
+    (cons 260 "#955f5f")
+    (cons 280 "#9e716b")
+    (cons 300 "#a78378")
+    (cons 320 "#b09685")
+    (cons 340 "#605a52")
+    (cons 360 "#605a52")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
