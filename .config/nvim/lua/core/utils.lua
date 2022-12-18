9bb659e6da00d62e2d953d90b1b6f8a3eee6cf0c
Dorovich <silverdovi25@gmail.com>
2022-12-18 21:48:17 +0100

    nvim

--- .config/nvim/lua/core/utils.lua
+++ .config/nvim/lua/core/utils.lua
@@ -7,22 +7,8 @@ SL = { silent = true }
 NRSL = { noremap = true, silent = true }
 VOID = {}
 
-function NMAP (first, second, opts)
-    keymap('n', first, second, opts)
-end
-
-function IMAP (first, second, opts)
-    keymap('i', first, second, opts)
-end
-
-function CMAP (first, second, opts)
-    keymap('c', first, second, opts)
-end
-
-function VMAP (first, second, opts)
-    keymap('v', first, second, opts)
-end
-
-function XMAP (first, second, opts)
-    keymap('x', first, second, opts)
-end
+function NMAP (first, second, opts) keymap('n', first, second, opts) end
+function IMAP (first, second, opts) keymap('i', first, second, opts) end
+function CMAP (first, second, opts) keymap('c', first, second, opts) end
+function VMAP (first, second, opts) keymap('v', first, second, opts) end
+function XMAP (first, second, opts) keymap('x', first, second, opts) end
