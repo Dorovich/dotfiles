0262cfa7608e5669344e0c9c44c23d1b805af8ba
Dorovich <silverdovi25@gmail.com>
2022-12-19 18:06:30 +0100

    firefox

--- .mozilla/firefox/ht3v7tl8.arkenfox/user-overrides.js
+++ .mozilla/firefox/ht3v7tl8.arkenfox/user-overrides.js
@@ -1,3 +1,4 @@
 user_pref("network.http.referer.XOriginPolicy", 0);
 user_pref("privacy.resistFingerprinting.letterboxing", false);
 user_pref("webgl.disabled", false);
+user_pref("keyword.enabled", true);
