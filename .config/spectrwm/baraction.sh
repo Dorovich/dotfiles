201bfdb816117a2260f10614b4a5f2c6695b842a
Dorovich <silverdovi25@gmail.com>
2023-02-12 17:18:37 +0100

    spectrwm

--- .config/spectrwm/baraction.sh
+++ .config/spectrwm/baraction.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
-my_clock() {
-    clock_status="$(date '%a %d de %b, %H:%M')"
-    echo "$clock_status"
-}
-
-while true; do
-    echo "$(my_clock)"
-    sleep 1m
-done
+# while true; do
+#     echo "?"
+#     sleep 1m
+# done
