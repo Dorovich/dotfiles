832018f01ce4f09d743b291d43013cb4f340ad76
Dorovich <silverdovi25@gmail.com>
2023-02-16 22:39:20 +0100

    cosas

--- .srcconfig/dmenu/config.h
+++ .srcconfig/dmenu/config.h
@@ -10,7 +10,8 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#abb2bf", "#151515" },
-	[SchemeSel] = { "#151515", "#d19a66" },
+	/*[SchemeSel] = { "#151515", "#d19a66" },*/ // Color amarillo
+	[SchemeSel] = { "#abb2bf", "#323232" }, // Color gris
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
