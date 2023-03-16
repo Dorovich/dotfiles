61475b0a371f8928ddabdfb0a254518212c6dbd9
Dorovich <silverdovi25@gmail.com>
2023-03-16 19:11:03 +0100

    nvim vanilla

---
+++ .config/nvim.vanilla/init.lua
@@ -0,0 +1,22 @@
+vim.opt.number = true
+vim.opt.laststatus = 0
+vim.opt.swapfile = false
+vim.opt.shiftwidth = 4
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.lazyredraw = true
+vim.opt.pumheight = 8
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.path = '.,/usr/include'
+vim.opt.mousemodel = 'extend'
+
+vim.g.mapleader = ','
+vim.g.c_syntax_for_h = true
+
+vim.keymap.set('n', 'ñ', ':', {})
+vim.keymap.set('i', 'jk', '<Esc>', {})
+vim.keymap.set('n', '<Leader>r', ':%s//g<Left><Left>', {})
+vim.keymap.set('n', '<Leader>t', ':split<CR>:term<CR>i', {})
+vim.keymap.set('n', '<Leader>c', ':!make<CR>', {})
+
+vim.api.nvim_create_autocmd("TermClose", { command = "call nvim_input('<CR>')" })
